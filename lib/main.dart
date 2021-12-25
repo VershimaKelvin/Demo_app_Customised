7a7ee4d4151671c7926552cedccb3f96b73a561e
Your Name <you@example.com>
2021-12-26 03:58:07 +0800

    i added the builder method to enable instanciate the provider instead of placing at the runnapp(MyApp()) place

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:untitled/Provider/provider.dart';
 
 void main() {
-  runApp( ChangeNotifierProvider(
-    create: (context)=>MyProvider(),
-    child: const MyApp(),
-  ),
+  runApp( const MyApp(),
   );
 }
 
@@ -16,37 +13,44 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    int value =Provider.of<MyProvider>(context).getCounter;
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title:  Center(child: Text('Provider '+ Provider.of<MyProvider>(context).getCounter.toString())),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
+    // int value =Provider.of<MyProvider>(context).getCounter;
+    return ChangeNotifierProvider(
+      create: (context)=>MyProvider(),
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: Scaffold(
+              appBar: AppBar(
+                title:  Center(child: Text('Provider '+ Provider.of<MyProvider>(context).getCounter.toString())),
               ),
-              Text(
-                Provider.of<MyProvider>(context).getCounter.toString(),
-                // Provider.of<MyProvider>(context).counter.toString(),
-                style: Theme.of(context).textTheme.headline4,
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'You have pushed the button this many times:',
+                    ),
+                    Text(
+                      Provider.of<MyProvider>(context).getCounter.toString(),
+                      // Provider.of<MyProvider>(context).counter.toString(),
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            Provider.of<MyProvider>(context, listen: false).incrementCounter();
-          },
-          child: const Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+              floatingActionButton: FloatingActionButton(
+                onPressed: (){
+                  Provider.of<MyProvider>(context, listen: false).incrementCounter();
+                },
+                child: const Icon(Icons.add),
+              ), // This trailing comma makes auto-formatting nicer for build methods.
+            ),
+          );
+        }
       ),
     );
   }
